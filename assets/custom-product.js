9433f82848cc7734803ce0d55cd04db00d58d601
Volodymyr <bryzulych@intelliboard.net>
2023-08-17 09:37:41 +0300

    17-08

--- assets/custom-product.js
+++ assets/custom-product.js
@@ -1,2 +1,6 @@
 let mainSection = $("[id^=MainProduct-template--]").parent('section');
 mainSection.css('background', 'linear-gradient(0deg, rgba(255,255,255,1) 53%, rgba(250,247,242,1) 95%, rgba(247,243,236,1) 100%)');
+
+$('.custom-product-faq .item-tab-title').click(function(){
+    $(this).parent().toggleClass('active');
+});
